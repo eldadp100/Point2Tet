a14ae89794992ff17ff77997773f973b4bc243d6
unia11 <uriadai@gmail.com>
2020-12-30 17:36:45 +0200

    Added main to losses.py

--- models/losses.py
+++ models/losses.py
@@ -1,7 +1,17 @@
 import torch
 from pytorch3d.loss import chamfer_distance
-
+import structures.QuarTet
 
 def chamfer_distance_quartet_to_point_cloud(quartet, pc, quartet_N_points=3000):
     quartet_pc = quartet.sample_point_cloud(quartet_N_points)
     return chamfer_distance(quartet_pc, pc)
+
+
+def main():
+    pc = torch.randn((3000, 3))
+    quartet = QuarTet()
+    chamfer_distance_quartet_to_point_cloud(quartet, pc)
+
+
+if __name__ == '__main__':
+    main()
