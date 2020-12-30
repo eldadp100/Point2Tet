b993aab2257ea6b71e2a4cfb9c1260db43b6dee5
eldadp100 <eldadperetz@mail.tau.ac.il>
2020-12-31 00:30:11 +0200

    ..

--- models/losses.py
+++ models/losses.py
@@ -1,12 +1,12 @@
-# import torch
-# from pytorch3d.loss import chamfer_distance
-#
-#
-# def chamfer_distance_quartet_to_point_cloud(quartet, pc, quartet_N_points=3000):
-#     quartet_pc = quartet.sample_point_cloud(quartet_N_points)
-#     return chamfer_distance(quartet_pc.unsqueeze(1), pc.unsqueeze(1))
-#
-# PUT IN COMMENT BEFORE PUSH TO GIT
 import torch
+from pytorch3d.loss import chamfer_distance
+
+
 def chamfer_distance_quartet_to_point_cloud(quartet, pc, quartet_N_points=3000):
-    return quartet.sample_point_cloud(quartet_N_points).abs().sum()
+    quartet_pc = quartet.sample_point_cloud(quartet_N_points)
+    return chamfer_distance(quartet_pc.unsqueeze(1), pc.unsqueeze(1))
+
+# # PUT IN COMMENT BEFORE PUSH TO GIT
+# import torch
+# def chamfer_distance_quartet_to_point_cloud(quartet, pc, quartet_N_points=3000):
+#     return quartet.sample_point_cloud(quartet_N_points).abs().sum()
