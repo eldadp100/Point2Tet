80314c79f5fc125f3d674b46bd483842838b87b4
eldadp100 <eldadperetz@mail.tau.ac.il>
2020-12-30 18:35:48 +0200

    ...

--- models/losses.py
+++ models/losses.py
@@ -4,4 +4,4 @@ from pytorch3d.loss import chamfer_distance
 
 def chamfer_distance_quartet_to_point_cloud(quartet, pc, quartet_N_points=3000):
     quartet_pc = quartet.sample_point_cloud(quartet_N_points)
-    return chamfer_distance(quartet_pc, pc)
+    return chamfer_distance(quartet_pc.unsqueeze(1), pc.unsqueeze(1))
