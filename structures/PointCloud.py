37b2c2527587d68b7ba2b9d8484b1c2acca2574b
unia11 <uriadai@gmail.com>
2020-12-31 17:54:52 +0200

    Added fill interior of point cloud method

--- structures/PointCloud.py
+++ structures/PointCloud.py
@@ -7,10 +7,18 @@ class PointCloud:
     def __iter__(self):
         return self.points.__iter__()
 
-    def fill_iterior_of_point_cloud(self, N):
+    def fill_iterior_of_point_cloud(self, N=9):
         """
             N: dense measurement
         """
+        multiple = float(1) / (N + 1)
+        curr_mult = 1 - multiple
+        original_points = self.points
+        for i in range(N):
+            new_points = original_points.copy()
+            self.points.extend(new_points * curr_mult)
+            curr_mult -= multiple
+
 
         # calculate SDF
         # sample from the whole cube N points and filter positive SDF points (leave only negative SDF)
