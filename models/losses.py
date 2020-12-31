bbb1bb39580b17a72a46a74be66d73340b099569
eldadp100 <eldadperetz@mail.tau.ac.il>
2020-12-31 19:04:30 +0200

    ...

--- models/losses.py
+++ models/losses.py
@@ -1,16 +1,22 @@
-import torch
-from chamferdist import ChamferDistance
+# import torch
+# from chamferdist import ChamferDistance
+#
+# def chamfer_dist(src_pc, dst_pc):
+#     chamferDist = ChamferDistance()
+#     src_pc = src_pc.type(torch.FloatTensor)
+#     dst_pc = dst_pc.type(torch.FloatTensor)
+#     dist_forward = chamferDist(src_pc, dst_pc)
+#     return dist_forward.detach().cpu().item()
+#
+# def chamfer_distance_quartet_to_point_cloud(quartet, pc, quartet_N_points=3000):
+#     quartet_pc = quartet.sample_point_cloud(quartet_N_points)
+#     return chamfer_distance(quartet_pc.unsqueeze(1), pc.unsqueeze(1))
 
-def chamfer_dist(src_pc, dst_pc):
-    chamferDist = ChamferDistance()
-    src_pc = src_pc.type(torch.FloatTensor)
-    dst_pc = dst_pc.type(torch.FloatTensor)
-    dist_forward = chamferDist(src_pc, dst_pc)
-    return dist_forward.detach().cpu().item()
-
+from pytorch3d.loss import chamfer_distance
 def chamfer_distance_quartet_to_point_cloud(quartet, pc, quartet_N_points=3000):
     quartet_pc = quartet.sample_point_cloud(quartet_N_points)
     return chamfer_distance(quartet_pc.unsqueeze(1), pc.unsqueeze(1))
+
 
 # # PUT IN COMMENT BEFORE PUSH TO GIT
 # import torch
@@ -18,9 +24,9 @@ def chamfer_distance_quartet_to_point_cloud(quartet, pc, quartet_N_points=3000):
 #
 # def chamfer_distance_quartet_to_point_cloud(quartet, pc, quartet_N_points=3000):
 #     return quartet.sample_point_cloud(quartet_N_points).abs().sum()
-
-
-if __name__ == '__main__':
-    a = torch.rand((1000, 3))
-    b = torch.rand((1400, 3))
-    chamfer_distance_quartet_to_point_cloud(a, b)
+#
+# import torch
+# if __name__ == '__main__':
+#     a = torch.rand((1000, 3))
+#     b = torch.rand((1400, 3))
+#     chamfer_distance_quartet_to_point_cloud(a, b)
