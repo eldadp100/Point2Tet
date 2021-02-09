795f08f50dc79a3ef94865ea4f0c4481b54b55f4
Eldad Peretz <eldadperetz@mail.tau.ac.il>
2021-02-09 13:46:37 +0200

    Update loss.py

--- src/loss.py
+++ src/loss.py
@@ -3,22 +3,34 @@ import torch
 from chamferdist import ChamferDistance
 
 
-def chamfer_dist(src_pc, dst_pc, src_weights, padding_ratio):
+#
+# def chamfer_dist(src_pc, dst_pc, src_weights, padding_ratio):
+#     chamferDist = ChamferDistance()
+#     src_pc = src_pc.type(torch.FloatTensor)
+#     dst_pc = dst_pc.type(torch.FloatTensor)
+#     src_weights = src_weights.type(torch.FloatTensor)
+#     dist = chamferDist.forward(src_pc, dst_pc, src_weights, padding_ratio)
+#     return dist
+
+def chamfer_dist(src_pc, dst_pc, src_occupancy, occ_weight=1.):
     chamferDist = ChamferDistance()
+    src_pc = torch.cat([src_pc, occ_weight * src_occupancy.unsqueeze(-1)], dim=2)
+    dst1_pc = torch.cat([dst_pc / 2., occ_weight * torch.zeros((dst_pc.shape[0], 1, 1))], dim=2)
+    dst2_pc = torch.cat([torch.rand((dst_pc.shape[0], 1, 3)), torch.ones((dst_pc.shape[0], 1, 1))], dim=2)
+    dst_pc = torch.cat([dst1_pc, dst2_pc], dim=0)
     src_pc = src_pc.type(torch.FloatTensor)
     dst_pc = dst_pc.type(torch.FloatTensor)
-    src_weights = src_weights.type(torch.FloatTensor)
-    dist = chamferDist.forward(src_pc, dst_pc, src_weights, padding_ratio)
+    dist = chamferDist.forward(src_pc, dst_pc)
     return dist
 
 
 def loss(quartet, pc, n=3000, lambda_1=1., lambda_2=0., lambda_3=0.):
     assert lambda_1 + lambda_2 + lambda_3 <= 1.
     lambda_4 = 1. - lambda_1 - lambda_2 - lambda_3
-    quartet_pc, weights, padding_ratio = quartet.sample_point_cloud(n)
+    quartet_pc, weights = quartet.sample_point_cloud(2 * n)
 
     # chamfer loss
-    loss_1 = chamfer_dist(quartet_pc.unsqueeze(1), pc.unsqueeze(1), weights, padding_ratio)
+    loss_1 = chamfer_dist(quartet_pc.unsqueeze(1), pc.unsqueeze(1), weights)
 
     # tets volumes loss
     # avg_vols = sum(volumes) / len(volumes)
